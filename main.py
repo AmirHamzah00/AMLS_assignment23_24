ee3e23b6d5c7f88c60eef8a55174a291eb4a2100
Amir Hamzah <test@gmail.com>
2024-01-10 16:09:01 +0000

    added folder and updated main.py

--- main.py
+++ main.py
@@ -1,26 +1,56 @@
+from A.TaskA import Load_Trained_CNN_Model_TaskA
+
 def Model_A():
     selection = True
     while selection:
+        print("\nTask A Selection Menu")
         print("\nPlease Select an Option:")
         print("[0] Train and Evalute the CNN Model")
         print("[1] Load and Evaluate Saved CNN Model")
-        print("[2] Exit")
+        print("[2] Exit to Main Menu")
         print()
         
         selection = input("Please Enter Key: ") 
         if selection =="0": 
-            print("Model A CNN Trained and Evaluated")
+            print("\nModel A CNN Trained and Evaluated")
+            
         elif selection =="1":
-            print("Saved Model A CNN Evaluated")
+            selection = True
+            while selection:
+                print("\nPlease Select an Option:")
+                print("[0] 1st Saved CNN Model")
+                print("[1] 2nd Saved CNN Model")
+                print("[2] 3rd Saved CNN Model")
+                print("[3] Exit to Previous Selection Menu")
+                print()
+                
+                selection = input("Please Enter Key: ") 
+                if selection =="0": 
+                    Load_Trained_CNN_Model_TaskA(1)
+                    print("\n1st Saved CNN Model Evaluated")
+                elif selection =="1":
+                    Load_Trained_CNN_Model_TaskA(2)
+                    print("\n2nd Saved CNN Model Evaluated")
+                elif selection =="2":
+                    Load_Trained_CNN_Model_TaskA(3)
+                    print("\n3rd Saved CNN Model Evaluated")
+                elif selection =="3":
+                    print('\nExited!')
+                    Model_A()
+                else:
+                    print("\nInvalid Choice. Re-eneter Key.")
+                    
         elif selection =="2":
-            print('\nExited!\n')
-            exit()
+            print('\nExited!')
+            selection_menu()
+            
         else:
             print("\nInvalid Choice. Re-eneter Key.") 
 
 def Model_B():
     selection = True
     while selection:
+        print("\nTask B Selection Menu")
         print("\nPlease Select an Option:")
         print("[0] Train and Evalute the CNN Model")
         print("[1] Load and Evaluate Saved CNN Model")
@@ -30,31 +60,56 @@ def Model_B():
         selection = input("Please Enter Key: ") 
         if selection =="0": 
             print("Model A CNN Trained and Evaluated")
+            
         elif selection =="1":
-            print("Saved Model A CNN Evaluated")
+            selection = True
+            while selection:
+                print("\nPlease Select an Option:")
+                print("[0] 1st Saved CNN Model")
+                print("[1] 2nd Saved CNN Model")
+                print("[2] 3rd Saved CNN Model")
+                print("[3] Exit to Previous Selection Menu")
+                print()
+                
+                selection = input("Please Enter Key: ") 
+                if selection =="0":
+                    print("\n1st Saved CNN Model Evaluated")
+                elif selection =="1":
+                    print("\n2nd Saved CNN Model Evaluated")
+                elif selection =="2":
+                    print("\n3rd Saved CNN Model Evaluated")
+                elif selection =="3":
+                    print('\nExited!')
+                    Model_B()
+                else:
+                    print("\nInvalid Choice. Re-eneter Key.")
+            
         elif selection =="2":
-            print('\nExited!\n')
+            print('\nExited!')
+            selection_menu()
+        else:
+            print("\nInvalid Choice. Re-eneter Key.")
+        
+def selection_menu():
+    selection = True
+    while selection:
+        
+        print("\nAMLS Assignment 23/24\n")
+        print("Please Select Which Task To Execute:")
+        print("[0] Execute Task A")
+        print("[1] Execute Task B")
+        print("[2] Exit")
+        print()
+        
+        selection = input("Please Enter Key: ") 
+        if selection =="0": 
+            Model_A() 
+        elif selection =="1":
+            Model_B() 
+        elif selection =="2":
+            print('\nMenu Exited!\n')
             exit()
         else:
             print("\nInvalid Choice. Re-eneter Key.") 
 
-selection = True
-while selection:
-    
-    print("\nAMLS Assignment 23/24\n")
-    print("Please Select Which Task To Execute:")
-    print("[0] Execute Task A")
-    print("[1] Execute Task B")
-    print("[2] Exit")
-    print()
-    
-    selection = input("Please Enter Key: ") 
-    if selection =="0": 
-        Model_A() 
-    elif selection =="1":
-        Model_B() 
-    elif selection =="2":
-        print('\nMenu Exited!\n')
-        exit()
-    else:
-      print("\nInvalid Choice. Re-eneter Key.") 
+selection_menu()
